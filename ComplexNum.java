808251e0ce996ccc865353ccac8fd7770606834f
AbramPatriarca <1110120@apps.nsd.org>
2025-06-18 11:38:36 -0700

    Complex Class Completed

--- ComplexNum.java
+++ ComplexNum.java
@@ -8,10 +8,32 @@ class ComplexNum {
         this.imaginary = imagine;
     }
 
-    
-
-    public void add(ComplexNum other) {
-
+    public double getReal() {
+        return real;
     }
+
+    public double getImagine() {
+        return imaginary;
+    }
+
+
+    public ComplexNum add(ComplexNum other) {
+        double realSum = this.getReal() + other.getReal();
+        double imagineSum = this.getImagine() + other.getImagine();
+        return new ComplexNum(realSum, imagineSum);
+    }
+
+    public ComplexNum sub(ComplexNum other) {
+        ComplexNum diff = this.add(new ComplexNum(-1 * other.getReal(), -1 * other.getImagine()));
+        return diff;
+    }
+
+    public ComplexNum mult(ComplexNum other) {
+        double realProduct = this.getReal() * other.getReal();
+        double imaginedProduct = this.getReal() * other.getImagine() + this.getImagine() * other.getReal() + this.getImagine() * other.getImagine();
+        return new ComplexNum(realProduct, imaginedProduct);
+    }
+
+    
 
 }
